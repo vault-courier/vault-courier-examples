fcbd26900719aae7e33d87d0d1f7b0e1dcea87c5
Javier Cuesta <jav.cuesta@gmail.com>
2025-05-02 23:24:00 +0200

    Add create dynamic role example

---
+++ create-dynamic-role-example/README.md
@@ -0,0 +1,75 @@
+#  Create Dynamic Role
+
+An example project using VaultCourier.
+
+This example is part of the tutorial [understanding dynamic roles](https://swiftpackageindex.com/vault-courier/vault-courier/main/tutorials/documentation/understand-dynamic-roles), section 3 and is a follow-up to [configure-pg-connection-example](https://github.com/vault-courier/vault-courier-examples/tree/main/configure-pg-connection-example). It's a CLI client that uses a configured connection between vault and a [Postgres](https://www.postgresql.org) database to generate dynamic credentials.
+
+## Usage
+
+> Important: This example is deliberately simplified and is intended for illustrative purposes only. In particular, never use a Vault instance in dev-mode in a production deployment, and consider using a dedicated vault root user when interacting with databases.
+
+Start your vault instance instance in dev-mode either with Hashicorp Vault
+
+```sh
+% vault server -dev -dev-root-token-id="education"
+```
+
+or with OpenBao
+
+```sh
+% bao server -dev -dev-root-token-id="education"
+```
+
+In another terminal spin up a PostgreSQL container with docker:
+
+```sh
+$ docker pull postgres:latest
+
+$ docker run \
+    --detach \
+    --name learn-postgres \
+    -e POSTGRES_USER=vault_root \
+    -e POSTGRES_PASSWORD=root_password \
+    -e POSTGRES_DB=postgres \
+    -p 5432:5432 \
+    --rm \
+    postgres
+
+$ docker exec -i \
+    learn-postgres \
+    psql -U vault_root -d postgres -c "CREATE ROLE \"read_only\" NOINHERIT;"
+
+$ docker exec -i \
+  learn-postgres \
+  psql -U vault_root -d postgres -c "GRANT SELECT ON ALL TABLES IN SCHEMA public TO \"read_only\";"
+```
+
+If not done before, run [configure-pg-connection-example](https://github.com/vault-courier/vault-courier-examples/tree/main/configure-pg-connection-example).
+
+Finally, build and run the VaultCourier client CLI using the same arguments as given to `configure-pg-connection-example`:
+
+```sh
+% swift build
+% $(swift build --show-bin-path)/configure-pg-connection-example -e "path/to/database/mount" -c "my_connection" -r "read_only"
+lease_id           path/to/database/mount/creds/read_only/TVo0C6muW9bzvlrREx8pZKKr
+lease_duration     3600
+lease_renewable    true
+password           52TkXJAM43E-G7IBgV9c
+username           v-token-read_onl-EkLbrLOfzIiGuIVmtEx9-1746220628 
+```
+
+You can verified that the user was created in the database with
+
+```sh
+% docker exec -i \
+  learn-postgres \
+  psql -U vault_root -d postgres -c "SELECT usename, valuntil FROM pg_user;"
+
+                     usename                      |        valuntil
+--------------------------------------------------+------------------------
+ vault_root                                       |
+ v-token-read_onl-EkLbrLOfzIiGuIVmtEx9-1746220628 | 2025-06-03 20:28:25+00
+
+```
+
+
---
+++ create-dynamic-role-example/Sources/Run.swift
@@ -0,0 +1,78 @@
+//===----------------------------------------------------------------------===//
+//  Copyright (c) 2025 Javier Cuesta
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//===----------------------------------------------------------------------===//
+
+import ArgumentParser
+import OpenAPIAsyncHTTPClient
+import VaultCourier
+#if canImport(FoundationEssentials)
+import FoundationEssentials
+#else
+import struct Foundation.URL
+#endif
+
+@main
+struct create_dynamic_role_example: AsyncParsableCommand {
+    @Option(name: .shortAndLong)
+    var enginePath: String = "path/to/database/mount" // "sql_database"
+
+    @Option(name: .shortAndLong)
+    var connectionName: String = "my-connection" //"pg_connection"
+
+    @Option(name: .shortAndLong)
+    var roleName: String = "read_only"
+
+    mutating func run() async throws {
+        let vaultClient = try Self.makeVaultClient()
+        try await vaultClient.authenticate()
+
+        let creationStatements = [
+            #"CREATE ROLE "{{name}}" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}' INHERIT;"#,
+            #"GRANT read_only TO "{{name}}";"#
+        ]
+        try await vaultClient.create(dynamicRole: .init(vaultRoleName: roleName,
+                                                        databaseConnectionName: connectionName,
+                                                        defaultTTL: "1h",
+                                                        maxTTL: "24h",
+                                                        creationStatements: creationStatements),
+                                     enginePath: enginePath)
+
+        let response = try await vaultClient.databaseCredentials(dynamicRole: roleName, enginePath: enginePath)
+        print("""
+        lease_id           \(response?.leaseId ?? "")
+        lease_duration     \(response?.leaseDuration ?? 0)
+        lease_renewable    \(response?.renewable ?? false)
+        password           \(response?.password ?? "")
+        username           \(response?.username ?? "")    
+        """)
+    }
+
+    static func makeVaultClient() throws -> VaultClient {
+        let vaultURL = URL(string: "http://127.0.0.1:8200/v1")!
+        let config = VaultClient.Configuration(apiURL: vaultURL)
+
+        let client = Client(
+            serverURL: vaultURL,
+            transport: AsyncHTTPClientTransport()
+        )
+
+        return VaultClient(
+            configuration: config,
+            client: client,
+            authentication: .token("education")
+        )
+    }
+}
+
